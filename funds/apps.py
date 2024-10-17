f76a0c4ed51349ca2359f9e60b4200db34e3d7c5
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-10-17 17:03:25 +0545

    chore: rename app fund to funds

--- funds/apps.py
+++ funds/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FundConfig(AppConfig):
+class FundsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "fund"
+    name = "funds"
