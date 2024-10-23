cc8fffac8d7473ec97db44e611047b390755246f
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-10-23 22:15:39 +0545

    feat: api endpoints for creating and listing of firms for the request user

--- v1/firms/serializers.py
+++ v1/firms/serializers.py
@@ -5,14 +5,18 @@ from .models import Firm
 
 class FirmSerializer(serializers.ModelSerializer):
     managers_count = serializers.SerializerMethodField()
-    clients_count = serializers.SerializerMethodField()
+    pool_investment_clients_count = serializers.SerializerMethodField()
+    general_clients_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Firm
-        fields = ['id', 'name', 'managers_count', 'clients_count']
+        fields = ['id', 'name', 'managers_count', 'pool_investment_clients_count', 'general_clients_count']
 
     def get_managers_count(self, obj):
         return obj.managers.count()
 
-    def get_clients_count(self, obj):
-        return obj.clients.count()
+    def get_pool_investment_clients_count(self, obj):
+        return obj.poolinvestmentclient_set.count()
+
+    def get_general_clients_count(self, obj):
+        return obj.generalclient_set.count()
