ff35777572e122f8d6df8369a2b09deced9c653d
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-10-31 14:41:07 +0545

    chore: admin panel for each tenant for dev purposes, will be reverted later

--- v1/firms/urls.py
+++ v1/firms/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 
 from .views import index
 
 urlpatterns = [
     path('', index, name='firm-index'),
+    path('admin/', admin.site.urls),
 ]
