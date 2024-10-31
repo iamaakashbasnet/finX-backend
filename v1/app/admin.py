46fea09ee69e19bf1b6d2b9b5adf2de19bf84f83
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-10-31 15:03:13 +0545

    feat: tenant admin panel

--- v1/app/admin.py
+++ v1/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from v1.app.models import Firm, Domain
+
+
+class TenantAdminSite(admin.AdminSite):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.register(Firm)
+        self.register(Domain)
+
+
+tenant_admin_site = TenantAdminSite(name='tenant_admin_site')
--- v1/app/urls.py
+++ v1/app/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from .views import index
+from .admin import tenant_admin_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin-tenant/', tenant_admin_site.urls),
     path('users/', include('v1.users.urls')),
     path('', index),
 ]
