237c260bab88d0354d4fc67e36eed7d6704d6114
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-10-31 14:21:13 +0545

    feat: firm model

--- v1/firms/models.py
+++ v1/firms/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Firm(models.Model):
+    name = models.CharField(max_length=255)
+    managers = models.ManyToManyField(get_user_model(), related_name='managed_firms', blank=True)
+
+    def __str__(self):
+        return self.name
