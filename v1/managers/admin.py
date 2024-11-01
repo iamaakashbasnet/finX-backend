e474f8d98cf479ff390c820c4238e6a33cb2f69c
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-11-01 12:05:02 +0545

    feat: managers model enhancements

--- v1/managers/admin.py
+++ v1/managers/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import FirmManager
+
+from .models import FirmManager, FirmExecutive
 
 
 @admin.register(FirmManager)
 class GeneralClientAdmin(admin.ModelAdmin):
-    list_display = ['user', 'is_active']
+    list_display = ['user', 'role_created_at', 'is_active']
+
+
+@admin.register(FirmExecutive)
+class FirmExecutiveAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role_created_at', 'is_active']
--- v1/managers/models.py
+++ v1/managers/models.py
@@ -5,8 +5,19 @@ from django.utils.translation import gettext_lazy as _
 
 class FirmManager(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    role_created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = _("Firm Manager")
         verbose_name_plural = _("Firm Managers")
+
+
+class FirmExecutive(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    role_created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Firm Executive")
+        verbose_name_plural = _("Firm Executives")
