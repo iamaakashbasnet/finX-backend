a8146fabe3be4423139cb030e7c2c1fec252489f
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-11-01 11:52:04 +0545

    feat: clients model enhancements

--- v1/clients/admin.py
+++ v1/clients/admin.py
@@ -5,9 +5,10 @@ from v1.clients.models import PoolInvestmentClient, GeneralClient
 
 @admin.register(PoolInvestmentClient)
 class PoolInvestmentClientAdmin(admin.ModelAdmin):
-    list_display = ('user', 'shares_amount', 'nav_value', 'is_active')
+    list_display = ['user', 'shares_amount', 'nav_value', 'payments', 'is_active',
+                    'joined_at']
 
 
 @admin.register(GeneralClient)
 class GeneralClientAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_active')
+    list_display = ['user', 'payments', 'is_active', 'joined_at']
--- v1/clients/models.py
+++ v1/clients/models.py
@@ -7,17 +7,27 @@ class PoolInvestmentClient(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     shares_amount = models.DecimalField(max_digits=10, decimal_places=2)
     nav_value = models.DecimalField(max_digits=10, decimal_places=2)
+    payments = models.DecimalField(max_digits=10, decimal_places=2)
+    joined_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = _("Pool Investment Client")
         verbose_name_plural = _("Pool Investment Clients")
 
+    def __str__(self):
+        return f'Pool Investment Client: {self.user.email}'
+
 
 class GeneralClient(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    payments = models.DecimalField(max_digits=10, decimal_places=2)
+    joined_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = _("General Client")
         verbose_name_plural = _("General Clients")
+
+    def __str__(self):
+        return f'General Client: {self.user.email}'
