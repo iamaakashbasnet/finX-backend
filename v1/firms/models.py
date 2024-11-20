9839546fd3cd3a963e88de73035f55a499d33742
Aakash Basnet <iamaakashbasnet@gmail.com>
2024-11-20 08:01:36 +0545

    feat: subscription flag for firm tenant model

--- v1/firms/models.py
+++ v1/firms/models.py
@@ -5,6 +5,7 @@ from django_tenants.models import TenantMixin, DomainMixin
 class Firm(TenantMixin):
     name = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
+    subscription_is_active = models.BooleanField(default=True)
 
 
 class Domain(DomainMixin):
--- v1/firms/views.py
+++ v1/firms/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from .models import Firm, Domain
 
 
 def index(request):
-    return HttpResponse(f'{request.tenant} index')
+    current_firm = get_object_or_404(Firm, schema_name=request.tenant)
+    current_domain = get_object_or_404(Domain, tenant=current_firm)
+    return HttpResponse(f''
+                        f'<h1>{request.tenant} index</h1>'
+                        f'<p>Firm: {current_firm.name}</p>'
+                        f'<p>Domain: {current_domain.domain}</p>'
+                        f'<p>Subscription: {current_firm.subscription_is_active}</p>')
