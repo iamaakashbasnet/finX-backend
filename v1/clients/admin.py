75b06e74d44b4f62acd3e3ae1aa798f4f0640ea9
Aakash Basnet <iamaakashbasnet@gmail.com>
2025-01-01 20:33:52 +0545

    refactor: removing get_nav_value as it is redundant

--- v1/clients/admin.py
+++ v1/clients/admin.py
@@ -5,12 +5,7 @@ from .models import PoolInvestmentClient, GeneralClient
 
 @admin.register(PoolInvestmentClient)
 class PoolInvestmentClientAdmin(admin.ModelAdmin):
-    readonly_fields = ('get_nav_value',)
-    list_display = ['user', 'shares_amount', 'get_nav_value', 'payments', 'is_active', 'joined_at']
-
-    @admin.display(description="NAV Value")
-    def get_nav_value(self, obj):
-        return obj.nav_value if obj.nav_value else "N/A"
+    list_display = ['user', 'shares_amount', 'nav_value', 'payments', 'is_active', 'joined_at']
 
 
 @admin.register(GeneralClient)
